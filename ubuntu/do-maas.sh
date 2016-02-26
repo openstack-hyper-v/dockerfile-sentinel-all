a516e92dabfe03b56de441cc665111fe862868ba
Sean Spratt <seansp@microsoft.com>
2016-02-26 16:46:33 -0500

    Updated Sentinel to former state. MAAS powers explicit.

--- ubuntu/do-maas.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/ppouliot/puppet-maas /etc/puppet/modules/maas
-sed "2d" -i /etc/puppet/modules/maas/manifests/init.pp
-puppet apply --debug --trace --verbose --modulepath=/etc/puppet/modules /etc/puppet/manifests 
--- ubuntu/site.pp
+++
@@ -1,7 +0,0 @@
-class{ 'maas':
-  maas_maintainers_release => 'stable',
-#  superuser { 'galera_admin':
-#    password => 'maas',
-#    email => 'ppouliot@microsoft.com',
-#    require => Package[ 'maas' ],
-}
